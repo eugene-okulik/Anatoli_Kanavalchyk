ad2c1c846ff9b35a079fb4d7dd8fdcebea4424e4
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-07-01 14:46:22 +0200

    Update task_1.py

--- homework/Homework_3/task_1.py
+++ homework/Homework_3/task_1.py
@@ -5,6 +5,7 @@ sum_ab = a + b
 diff_ab = a - b
 mult_ab = a * b
 
+
 print(f"Сумма {a} и {b} равна: {sum_ab}")
 print(f"Разница {a} и {b} равна: {diff_ab}")
 print(f"Произведение {a} и {b} равно: {mult_ab}")
