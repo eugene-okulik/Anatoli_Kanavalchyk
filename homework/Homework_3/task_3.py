934f117cc53019b7d100c4b4d962affde6e7076d
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-07-01 14:47:00 +0200

    Update task_3.py

--- homework/Homework_3/task_3.py
+++ homework/Homework_3/task_3.py
@@ -4,6 +4,6 @@ y = 4
 average_arithmetic = (x + y) / 2
 average_geometric = (x * y) ** 0.5
 
+
 print(f"Среднее арифметическое {x} и {y} равно: {average_arithmetic}")
 print(f"Среднее геометрическое {x} и {y} равно: {average_geometric}")
-
