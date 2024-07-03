a4460ce4a211ba82fdd7c1fe87daecb428b59fda
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-07-03 11:55:50 +0200

    Update task_3_text.py
    Сделал с помощью join

--- homework/Homework_5/task_3_text.py
+++ homework/Homework_5/task_3_text.py
@@ -3,8 +3,6 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-text = f"Students {students[0]}, {students[1]}, {students[2]} " \
-       f"study these subjects: {subjects[0]}, " \
-       f""f"{subjects[1]}, "f"{subjects[2]}"
+text = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}"
 
 print(text)
