7a4f506ca81d7deee63942818d2e0242d60629fd
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-08-05 15:42:10 +0200

    Update test_task_2_playwright.py

--- homework/Homework_27/test_task_2_playwright.py
+++ homework/Homework_27/test_task_2_playwright.py
@@ -1,36 +1,16 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 
 
-def test_new_tab_interaction(browser_name):
-    with sync_playwright() as p:
-        if browser_name == 'chromium':
-            browser = p.chromium.launch()
-        elif browser_name == 'firefox':
-            browser = p.firefox.launch()
-        elif browser_name == 'webkit':
-            browser = p.webkit.launch()
-        else:
-            raise ValueError(f"Unsupported browser: {browser_name}")
+def test_new_tab_interaction(page: Page):
+    page.goto('https://www.qa-practice.com/elements/new_tab/button')
 
-        context = browser.new_context()
-        page = context.new_page()
+    button = page.locator('#new-page-button')
+    button.click()
 
-        page.goto('https://www.qa-practice.com/elements/new_tab/button')
+    new_tab = page.context.wait_for_event('page')
 
-        button = page.locator('#new-page-button')
-        button.click()
+    expect(new_tab.locator('#result-text')).to_have_text('I am a new page in a new tab')
 
-        new_tab = context.wait_for_event('page')
+    expect(button).to_be_enabled()
 
-        expect(new_tab.locator('#result-text')).to_have_text('I am a new page in a new tab')
-
-        expect(button).to_be_enabled()
-
-        new_tab.close()
-        page.close()
-        context.close()
-        browser.close()
-
-
-for browser_type in ['chromium', 'firefox', 'webkit']:
-    test_new_tab_interaction(browser_type)
+    new_tab.close()
