beeecb6df7bb4889d989d211acf9619280e1b7a6
Anatoli4441 <160398483+Anatoli4441@users.noreply.github.com>
2024-08-05 15:41:41 +0200

    Update test_task_3_playwright.py

--- homework/Homework_27/test_task_3_playwright.py
+++ homework/Homework_27/test_task_3_playwright.py
@@ -1,30 +1,11 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
 
-def test_color_change(browser_name):
-    with sync_playwright() as p:
-        if browser_name == 'chromium':
-            browser = p.chromium.launch()
-        elif browser_name == 'firefox':
-            browser = p.firefox.launch()
-        elif browser_name == 'webkit':
-            browser = p.webkit.launch()
-        else:
-            raise ValueError(f"Unsupported browser: {browser_name}")
+def test_color_change(page: Page):
+    page.goto('https://demoqa.com/dynamic-properties')
 
-        context = browser.new_context()
-        page = context.new_page()
+    color_change_button = page.locator('button#colorChange')
 
-        page.goto('https://demoqa.com/dynamic-properties')
+    page.wait_for_selector('button#colorChange.text-danger')
 
-        color_change_button = page.locator('button#colorChange')
-
-        page.wait_for_selector('button#colorChange.text-danger')
-
-        color_change_button.click()
-
-        browser.close()
-
-
-for browser_type in ['chromium', 'firefox', 'webkit']:
-    test_color_change(browser_type)
+    color_change_button.click()
